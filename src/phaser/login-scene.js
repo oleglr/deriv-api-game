5232c6ce7dd0e5caa2ff153b84cdbb0776d72c1e
oleglr <oleglr@mail.ru>
2025-01-17 15:38:02 +0300

    Update login-scene.js

--- src/phaser/login-scene.js
+++ src/phaser/login-scene.js
@@ -1,7 +1,7 @@
-// /* eslint-disable no-restricted-globals*/
+// /* eslint-disable no-restricted-globals */
 import Phaser from 'phaser';
 
-// import Authorize API data
+// Import Authorize API data
 import getUserAuthorization from '../api/authorize';
 
 const COLOR_PRIMARY = 0x83580b;
@@ -13,28 +13,30 @@ class LoginScene extends Phaser.Scene {
     }
 
     create() {
-        let print = this.add.text(0, 0, '');
+        // Создание фона
         const background = this.add.sprite(0, 0, 'title_background');
         background.setOrigin(0, 0);
         background.setDisplaySize(this.game.config.width, this.game.config.height);
+
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
 
-        let loginDialog = CreateLoginDialog(this, {
+        // Создание диалогового окна входа
+        const loginDialog = CreateLoginDialog(this, {
             x: screenCenterX,
             y: 450,
             title: 'Deriv',
             error: 'error',
             userToken: '',
         })
-            .on('login', function (userToken) {
+            .on('login', (userToken) => {
                 getUserAuthorization(userToken)
-                    .then(response => {
+                    .then((response) => {
                         sessionStorage.setItem('user_token', userToken);
                         sessionStorage.setItem('user_email', response.authorize.email);
 
                         showTitlePage();
                     })
-                    .catch(err => {
+                    .catch((err) => {
                         alert(err.error.message);
                         sessionStorage.removeItem('user_token');
                         sessionStorage.removeItem('user_email');
@@ -42,6 +44,7 @@ class LoginScene extends Phaser.Scene {
             })
             .popUp(500);
 
+        // Добавление сообщения
         this.message = this.add
             .text(screenCenterX, 250, 'Submit your Deriv API token for playing the game', {
                 color: '#FFFFFF',
@@ -51,28 +54,33 @@ class LoginScene extends Phaser.Scene {
             })
             .setOrigin(0.5);
 
+        // Переключение на TitleScene
         const showTitlePage = () => {
             this.scene.switch('TitleScene');
         };
     }
 
-    update() {}
+    update() {
+        // Обновление сцены (если необходимо)
+    }
 }
 
 const GetValue = Phaser.Utils.Objects.GetValue;
-let CreateLoginDialog = function (scene, config, onSubmit) {
+
+// Функция для создания диалогового окна входа
+const CreateLoginDialog = (scene, config) => {
     let userToken = GetValue(config, 'userToken', '');
-    let title = GetValue(config, 'title', 'Deriv');
+    const title = GetValue(config, 'title', 'Deriv');
 
-    let x = GetValue(config, 'x', 0);
-    let y = GetValue(config, 'y', 0);
-    let width = GetValue(config, 'width', undefined);
-    let height = GetValue(config, 'height', undefined);
+    const x = GetValue(config, 'x', 0);
+    const y = GetValue(config, 'y', 0);
+    const width = GetValue(config, 'width', undefined);
+    const height = GetValue(config, 'height', undefined);
 
-    let background = scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10, COLOR_PRIMARY);
-    let titleField = scene.add.text(0, 0, title);
+    const background = scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10, COLOR_PRIMARY);
+    const titleField = scene.add.text(0, 0, title);
 
-    let userTokenField = scene.rexUI.add
+    const userTokenField = scene.rexUI.add
         .label({
             orientation: 'x',
             background: scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10).setStrokeStyle(2, COLOR_LIGHT),
@@ -82,10 +90,10 @@ let CreateLoginDialog = function (scene, config, onSubmit) {
         })
         .setInteractive()
         .on('pointerdown', () => {
-            let config = {
+            const config = {
                 type: 'text',
                 text: userToken,
-                onTextChanged: function (textObject, text) {
+                onTextChanged: (textObject, text) => {
                     userToken = text;
                     textObject.text = text;
                 },
@@ -93,7 +101,7 @@ let CreateLoginDialog = function (scene, config, onSubmit) {
             scene.rexUI.edit(userTokenField.getElement('text'), config);
         });
 
-    let submitButton = scene.rexUI.add
+    const submitButton = scene.rexUI.add
         .label({
             orientation: 'x',
             background: scene.rexUI.add.roundRectangle(0, 0, 10, 10, 10, COLOR_LIGHT),
@@ -101,11 +109,11 @@ let CreateLoginDialog = function (scene, config, onSubmit) {
             space: { top: 8, bottom: 8, left: 8, right: 8 },
         })
         .setInteractive({ useHandCursor: true })
-        .on('pointerdown', function () {
+        .on('pointerdown', () => {
             loginDialog.emit('login', userToken);
         });
 
-    let loginDialog = scene.rexUI.add
+    const loginDialog = scene.rexUI.add
         .sizer({
             orientation: 'y',
             x: x,
@@ -118,6 +126,7 @@ let CreateLoginDialog = function (scene, config, onSubmit) {
         .add(userTokenField, 0, 'left', { bottom: 30, left: 10, right: 10 }, true)
         .add(submitButton, 0, 'center', { bottom: 20, left: 10, right: 10 }, false)
         .layout();
+
     return loginDialog;
 };
 
