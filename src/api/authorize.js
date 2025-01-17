91e38ab666a6df58410200b58e938b9b12aa084d
oleglr <oleglr@mail.ru>
2025-01-17 15:17:57 +0300

    appid

--- src/api/authorize.js
+++ src/api/authorize.js
@@ -1,6 +1,6 @@
 const DerivAPIBasic = require('@deriv/deriv-api/dist/DerivAPIBasic');
 
-const api = new DerivAPIBasic({ endpoint: 'frontend.binaryws.com', lang: 'EN', app_id: 31635 });
+const api = new DerivAPIBasic({ endpoint: 'ws.derivws.com', lang: 'EN', app_id: 67219 });
 
 export default async function getUserAuthorization(token) {
     return await api.authorize(token);
